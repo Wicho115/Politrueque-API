131d61c02dbaa52aeaded2be7f280a9e703ebe93
Wicho115 <martin.velazquez.luis.alfonso@gmail.com>
2021-05-14 17:55:32 -0500

    CORS policy

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,9 @@ import * as cookieParser from 'cookie-parser';
 const port = process.env.PORT || 4000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);  
+  const app = await NestFactory.create(AppModule, {cors : true});  
   app.use(cookieParser());
+  
   await app.listen(port);
   console.log(`Running on port ${port}`);  
   
